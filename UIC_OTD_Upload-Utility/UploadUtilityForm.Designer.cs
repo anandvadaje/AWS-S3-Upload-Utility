d4d51684e3bef0afa68ee6424ddc201f938dfa75
Anand Vadaje <avadaje1@binghamton.edu>
2017-07-15 01:47:50 -0400

    Progress Bar Implemented [finally :)] using background worker and removed unwanted classes and forms

--- UIC_OTD_Upload-Utility/UploadUtilityForm.Designer.cs
+++ UIC_OTD_Upload-Utility/UploadUtilityForm.Designer.cs
@@ -35,9 +35,11 @@
             this.button1 = new System.Windows.Forms.Button();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
-            this.label5 = new System.Windows.Forms.Label();
+            this.backgroundWorker = new System.ComponentModel.BackgroundWorker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -46,7 +48,7 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(90, 66);
+            this.label1.Location = new System.Drawing.Point(90, 31);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(92, 17);
             this.label1.TabIndex = 0;
@@ -58,7 +60,7 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(90, 136);
+            this.label2.Location = new System.Drawing.Point(90, 73);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(63, 17);
             this.label2.TabIndex = 1;
@@ -69,22 +71,22 @@
             this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox1.Location = new System.Drawing.Point(243, 61);
+            this.textBox1.Location = new System.Drawing.Point(243, 31);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(534, 22);
+            this.textBox1.Size = new System.Drawing.Size(578, 22);
             this.textBox1.TabIndex = 2;
-            this.textBox1.Text = "uic-otd-utility/test2";
+            this.textBox1.Text = "ananduic/test2";
             // 
             // textBox2
             // 
             this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox2.Location = new System.Drawing.Point(243, 136);
+            this.textBox2.Location = new System.Drawing.Point(243, 73);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(534, 22);
+            this.textBox2.Size = new System.Drawing.Size(578, 22);
             this.textBox2.TabIndex = 3;
-            this.textBox2.Text = "C:\\\\LineManager.zip";
+            this.textBox2.Text = "C:\\\\soln.txt";
             // 
             // button1
             // 
@@ -92,9 +94,9 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(597, 305);
+            this.button1.Location = new System.Drawing.Point(619, 180);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(180, 53);
+            this.button1.Size = new System.Drawing.Size(202, 67);
             this.button1.TabIndex = 4;
             this.button1.Text = "Upload File";
             this.button1.UseVisualStyleBackColor = true;
@@ -102,9 +104,9 @@
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(243, 256);
+            this.textBox3.Location = new System.Drawing.Point(243, 135);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(534, 22);
+            this.textBox3.Size = new System.Drawing.Size(578, 22);
             this.textBox3.TabIndex = 6;
             // 
             // label3
@@ -113,45 +115,56 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(90, 259);
+            this.label3.Location = new System.Drawing.Point(90, 140);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(99, 17);
             this.label3.TabIndex = 7;
             this.label3.Text = "Uploaded Link";
             // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(243, 106);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(578, 23);
+            this.progressBar1.TabIndex = 8;
+            // 
+            // backgroundWorker
+            // 
+            this.backgroundWorker.WorkerReportsProgress = true;
+            this.backgroundWorker.WorkerSupportsCancellation = true;
+            this.backgroundWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker_DoWork);
+            this.backgroundWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker_ProgressChanged);
+            this.backgroundWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker_RunWorkerCompleted);
+            // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(153, 216);
+            this.label4.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(93, 212);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(0, 20);
-            this.label4.TabIndex = 8;
+            this.label4.Size = new System.Drawing.Size(0, 29);
+            this.label4.TabIndex = 9;
             // 
-            // progressBar1
+            // pictureBox1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(93, 177);
-            this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(684, 23);
-            this.progressBar1.TabIndex = 9;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(436, 216);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(0, 20);
-            this.label5.TabIndex = 10;
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::UIC_OTD_Upload_Utility.Properties.Resources.scanningwoohoo;
+            this.pictureBox1.Location = new System.Drawing.Point(308, 59);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(185, 134);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
             // 
             // UploadUtlityForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(922, 387);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.progressBar1);
+            this.ClientSize = new System.Drawing.Size(851, 277);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label4);
+            this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.button1);
@@ -162,6 +175,7 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "UploadUtlityForm";
             this.Text = "UIC OTD Upload Utility";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -176,9 +190,10 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
         private System.Windows.Forms.ProgressBar progressBar1;
-        private System.Windows.Forms.Label label5;
+        private System.ComponentModel.BackgroundWorker backgroundWorker;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- UIC_OTD_Upload-Utility/UploadUtilityForm.cs
+++ UIC_OTD_Upload-Utility/UploadUtilityForm.cs
@@ -9,33 +9,30 @@ namespace UIC_OTD_Upload_Utility
 {
     public partial class UploadUtlityForm : Form
     {
-        UploadProgressForm progressForm;
+        string bucket = "";
+        string file = "";
+        static string url = "";
 
         public UploadUtlityForm()
         {
-            progressForm = new UploadProgressForm();
-            InitializeComponent();
+           InitializeComponent();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            progressForm.Show();
-
-            string bucket = textBox1.Text;
-            string file = textBox2.Text;
-
+            bucket = textBox1.Text;
+            file = textBox2.Text;
             Stopwatch watch = Stopwatch.StartNew();
+            pictureBox1.Visible = true;
 
-            Upload(bucket, file);
+            backgroundWorker.RunWorkerAsync();
 
             watch.Stop();
             TimeSpan ts = watch.Elapsed;
             string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
-
-            label5.Text = "Time Take to Upload = " + elapsedTime;
         }
 
-        public void Upload(string bucket, string file)
+        public string Upload(string bucket, string file)
         {
             string bucketName = bucket;
             string filePath = file;
@@ -55,26 +52,21 @@ namespace UIC_OTD_Upload_Utility
                         FilePath = filePath,
                     };
 
-                uploadRequest.UploadProgressEvent += new EventHandler<UploadProgressArgs>(uploadRequest_UploadPartProgressEvent);
+                uploadRequest.UploadProgressEvent += new EventHandler<UploadProgressArgs>(uploadProgress);
 
                 fileTransferUtility.Upload(uploadRequest);
 
                 GetPreSignedUrlRequest request1 = new GetPreSignedUrlRequest
                 {
                     BucketName = bucketName,
-                    Key = "Tax presentation.pdf",
+                    Key = "soln.txt",
                     Expires = DateTime.Now.AddMinutes(200)
                 };
-
-                progressForm.label2.Text = "File Upload completed";
-
+                
                 string urlString = client.GetPreSignedURL(request1);
-
-                textBox3.Text = urlString;
-
                 Console.WriteLine("File Upload completed");
 
-                progressForm.Hide();
+                return urlString;
             }
             catch (AmazonS3Exception amazonS3Exception)
             {
@@ -87,28 +79,36 @@ namespace UIC_OTD_Upload_Utility
                     throw new Exception("Error occurred: " + amazonS3Exception.Message);
                 }
             }
+
+            void uploadProgress(object sender, UploadProgressArgs e)
+            {
+                int pctProgress = (int)(e.TransferredBytes * 100 / e.TotalBytes);
+                backgroundWorker.ReportProgress(pctProgress);
+                
+                Console.WriteLine(e.TransferredBytes + " / " + e.TotalBytes + Environment.NewLine);
+            }
         }
 
-        void uploadRequest_UploadPartProgressEvent(object sender, UploadProgressArgs e)
+        private void backgroundWorker_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
         {
-            if (progressBar1 == null)
+            url = Upload(bucket, file);
+        }
+
+        private void backgroundWorker_ProgressChanged(object sender, System.ComponentModel.ProgressChangedEventArgs e)
+        {
+            if (pictureBox1.Visible)
             {
-                Console.WriteLine("{0}/{1}", e.TransferredBytes, e.TotalBytes);
+                pictureBox1.Visible = false;
             }
-            else
-            {
-                if (InvokeRequired)
-                {
-                    object[] args = new object[] { sender, e };
-                    Invoke((MethodInvoker)delegate { uploadRequest_UploadPartProgressEvent(sender, e); } );
-                }
-                else
-                {
-                    int val = (int)((e.TransferredBytes / e.TotalBytes) * 100);
-                    progressForm.progressBar1.Value = val;
-                    progressForm.label1.Text = String.Format("{0}% Upload Done", val);
-                }
-            }
+                
+            progressBar1.Value = e.ProgressPercentage;
+            label4.Text = e.ProgressPercentage + "% Upload Completed .....";
+        }
+
+        private void backgroundWorker_RunWorkerCompleted(object sender, System.ComponentModel.RunWorkerCompletedEventArgs e)
+        {
+            label4.Text = "File Upload Completed !!";
+            textBox3.Text = url;
         }
     }
 }
